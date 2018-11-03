278bb924c8b405ebc0f0ab2dbf4c45a5c5d9bf3b
yurii-kurylenko <yuriy.kurilenko7@gmail.com>
2018-11-03 23:53:18 +0200

    Allow database URL

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,15 @@ export default {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   databaseUrl: () => {
-    let url = "postgres://";
-    if (process.env.DB_USERNAME) { url += process.env.DB_USERNAME + ":"; }
-    if (process.env.DB_PASSWORD) { url += ":" + process.env.DB_PASSWORD; }
-    url += "@" + process.env.DB_HOST;
-    url += "/" + process.env.DB_NAME;
-    return url;
+    if (process.env.DATABASE_URL) {
+      return process.env.DATABASE_URL;
+    } else {
+      let url = "postgres://";
+      if (process.env.DB_USERNAME) { url += process.env.DB_USERNAME + ":"; }
+      if (process.env.DB_PASSWORD) { url += ":" + process.env.DB_PASSWORD; }
+      url += "@" + process.env.DB_HOST;
+      url += "/" + process.env.DB_NAME;
+      return url;
+    }
   }
 };
