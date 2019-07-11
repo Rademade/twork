0b9007f263278edb999ac68d364eb9ab98ac1bc0
Yurii Kurylenko <yuriy.kurilenko7@gmail.com>
2019-07-11 09:38:58 +0300

    add hardcodded push vapid

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -2,5 +2,5 @@ export default {
   apiPrefix: "/api/v1",
   googleAuthProviderLink: '/api/v1/auth/google',
   isProduction: process.env.NODE_ENV == 'production',
-  WebPushVapidPublicKey: process.env.VUE_APP_WEB_PUSH_VAPID_PUBLIC_KEY
+  WebPushVapidPublicKey: 'BC-2UVu0tn7q06TbBdBGB2QqPWmU0sZLJOQL_PmIpyfJ9PR6Lyycg2bfiivBiD_V9zrrZvX-PdgJW7aYA_WlLCg'
 };
